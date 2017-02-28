fd665ec1fe72f3f8dd877d09282881b664a962f1
josephquested <josephquested@gmail.com>
2017-02-28 21:16:26 +1300

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public float cameraHeight;
+	// the height of the camera above the playerTransform
+
+	Transform playerTransform;
+	// variable for storing the players transform
+
+	void Start ()
+	{
+		playerTransform = GameObject.FindWithTag("Player").transform;
+		// gets a reference to the player by using the FindWithTag method
+	}
+
+	void Update ()
+	{
+		UpdatePosition();
+		// will update the camera position every frame
+	}
+
+	void UpdatePosition ()
+	{
+		Vector3 position = new Vector3(
+			playerTransform.position.x,
+			playerTransform.position.y + cameraHeight,
+			playerTransform.position.z - 10
+		);
+		// declares a variable for the new camera position
+		// notice the ugly way you do multi-line Vector3s
+
+		transform.position = position;
+		// sets the camera position
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	// public variable used to assign speed to the players movement
+
+	public float jumpPower;
+	// remember: public variables can be set in the Unity Inspector
+
+	Rigidbody rb;
+	// the convention is to declare rigidbody references as 'rb'
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		// assigns the rb variable using the GetComponent method
+	}
+
+	void Update ()
+	{
+		MovementInput();
+		// updates the player movement every frame
+
+		JumpInput();
+		// same for jump input
+	}
+
+	void MovementInput ()
+	{
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		// declares a variable for movement, based on input axis. "GetAxisRaw" can also be used,
+		// for more immediate and tactile movement. GetAxisRaw is binary, as opposed to incremental
+
+		rb.AddForce(movement * speed);
+	}
+
+	void JumpInput ()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			rb.AddForce(Vector3.up * jumpPower);
+			// look at that! Vector3 has several useful in-built properties for you to use
+			// Vector3.up, Vector3.right, Vector3.down etc.
+		}
+	}
+}
